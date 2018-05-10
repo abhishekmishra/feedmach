23221e938b0b2ffff1741bf78bbca3b638933382
Abhishek Mishra <abhishekmishra3@gmail.com>
2018-05-10 17:09:15 +0530

    added schema creation

--- src/main/scripts/test.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS Feeds ( Id NOT NULL INTEGER PRIMARY KEY
-                                                             ,  Name NOT NULL TEXT, Url TEXT NOT NULL UNIQUE
-                                                                                                    ,  Added_date NOT NULL NUMERIC )
