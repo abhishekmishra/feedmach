c618c5a854d87f27e44bbf1f156f524a4e473c37
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-02-05 23:52:04 +0530

    using commander to build a cli

--- index.mjs
+++ index.mjs
@@ -9,9 +9,34 @@
 import { get } from 'http';
 import { createWriteStream } from 'fs';
 import { join, basename } from 'path';
+import { Command } from 'commander';
 
+// create the program object
+const program = new Command();
+
+// configure the cli program name and options
+program
+    .name('feedmach')
+    .description(`feedmach is a CLI to download, extract, organize and read` +
+        `RSS/ATOM feeds.`)
+    .version('0.1.0');
+
+program.command('add')
+  .description('Subscribe to a new feed.')
+  .argument('<string>', 'the feed url')
+  .action((str, options) => {
+    const limit = options.first ? 1 : undefined;
+    console.log(`are you trying to add the feed "${str}"?`);
+  });
+
+program.parse();
+
+
+// the download folder
+// TODO: this should be loaded from config/environment
 const downloadFolder = '.work';
 
+// download the feed at the given url
 function downloadFeed(url)
 {
     let fileName = basename(url);
@@ -23,4 +48,5 @@ function downloadFeed(url)
     });
 }
 
-downloadFeed('http://localhost:8000/feeds/all.rss.xml');
+// downloadFeed('http://localhost:8000/feeds/all.rss.xml');
+
