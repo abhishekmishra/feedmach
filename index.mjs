139c6cf3d96b80f910b6730f4eebc3322f4e03c7
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-02-06 00:25:47 +0530

    download and parse feed

--- index.mjs
+++ index.mjs
@@ -7,30 +7,10 @@
  * feedmach is a CLI to download, extract and organize rss feeds.
  */
 import { get } from 'http';
-import { createWriteStream } from 'fs';
+import { createWriteStream, readFileSync } from 'fs';
 import { join, basename } from 'path';
 import { Command } from 'commander';
-
-// create the program object
-const program = new Command();
-
-// configure the cli program name and options
-program
-    .name('feedmach')
-    .description(`feedmach is a CLI to download, extract, organize and read` +
-        `RSS/ATOM feeds.`)
-    .version('0.1.0');
-
-program.command('add')
-  .description('Subscribe to a new feed.')
-  .argument('<string>', 'the feed url')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(`are you trying to add the feed "${str}"?`);
-  });
-
-program.parse();
-
+import * as htmlparser2 from 'htmlparser2';
 
 // the download folder
 // TODO: this should be loaded from config/environment
@@ -46,7 +26,32 @@ function downloadFeed(url)
         response.pipe(file);
         console.log('downloaded ' + filePath);
     });
+    return filePath;
 }
+
+// create the program object
+const program = new Command();
+
+// configure the cli program name and options
+program
+    .name('feedmach')
+    .description(`feedmach is a CLI to download, extract, organize and read` +
+        `RSS/ATOM feeds.`)
+    .version('0.1.0');
+
+program.command('add')
+  .description('Subscribe to a new feed.')
+  .argument('<string>', 'the feed url')
+  .action((url, options) => {
+    const limit = options.first ? 1 : undefined;
+    console.log(`are you trying to add the feed "${url}"?`);
+    let feedFile = downloadFeed(url);
+    let feedContents = readFileSync(feedFile, 'utf8');
+    let parseContent = htmlparser2.parseFeed(feedContents);
+    console.log(`feed name is ${parseContent.title}`);
+  });
+
+program.parse();
 
 // downloadFeed('http://localhost:8000/feeds/all.rss.xml');
 
