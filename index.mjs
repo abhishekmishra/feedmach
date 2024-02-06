665b015041af0a50c1bbab16d78303eecbfdc376
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-02-06 11:08:25 +0530

    using fetch instead of get and handling feed reading in the callback

--- index.mjs
+++ index.mjs
@@ -6,8 +6,8 @@
  * 
  * feedmach is a CLI to download, extract and organize rss feeds.
  */
-import { get } from 'http';
-import { createWriteStream, readFileSync } from 'fs';
+// import { get } from 'http';
+import { createWriteStream, readFileSync, writeFileSync } from 'fs';
 import { join, basename } from 'path';
 import { Command } from 'commander';
 import * as htmlparser2 from 'htmlparser2';
@@ -22,10 +22,20 @@ function downloadFeed(url) {
   let fileName = basename(url);
   let filePath = join(downloadFolder, fileName);
   const file = createWriteStream(filePath);
-  const request = get(url, function (response) {
-    response.pipe(file);
+
+  fetch(url)
+  .then(response => response.text())
+  .then(data => {
+    // console.log(data);
+    writeFileSync(filePath, data);
     console.log(chalk.hex('#FFA500')('downloaded ' + filePath));
-  });
+    let feedContents = readFileSync(filePath, 'utf8');
+    // console.log(feedContents);
+    let parseContent = htmlparser2.parseFeed(feedContents);
+    console.log(chalk.black.bgGreen(`feed name is ${parseContent.title}`));
+  })
+  .catch(error => console.error('Error:', error));
+
   return filePath;
 }
 
@@ -46,9 +56,6 @@ program.command('add')
     const limit = options.first ? 1 : undefined;
     console.log(chalk.red.bgBlue.bold(`are you trying to add the feed "${url}"?`));
     let feedFile = downloadFeed(url);
-    let feedContents = readFileSync(feedFile, 'utf8');
-    let parseContent = htmlparser2.parseFeed(feedContents);
-    console.log(chalk.black.bgGreen(`feed name is ${parseContent.title}`));
   });
 
 program.command('list')
